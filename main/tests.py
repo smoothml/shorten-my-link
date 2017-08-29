05873d20ae1856d4712300fae653414fcb30aa35
Paul Harrison <paulharrison2@gmail.com>
2017-08-29 21:59:11 +0100

    Add create short URL test.

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import Urls
 
-# Create your tests here.
+
+class UrlsTestCase(TestCase):
+
+    def test_model_can_create_shortened_url(self):
+        old_count = Urls.objects.count()
+
+        long_url = 'www.batman.com'
+        url = Urls(url=long_url)
+        url.save()
+
+        new_count = Urls.objects.count()
+        self.assertEqual(new_count - old_count, 1)
