ad02219fdd5e9d58484239fc19a3e9590dee1750
Paul Harrison <paulharrison2@gmail.com>
2017-08-30 08:59:39 +0100

    Add redirect endpoint.

--- api/views.py
+++ api/views.py
@@ -1,9 +1,13 @@
 import os
 
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
 from .serializers import UrlsSerializer
+from .models import Urls
 
 APP_BASE_URL = 'https://shorten-my-link.herokuapp.com'
 
@@ -16,3 +20,9 @@ def create_short_url(request):
         return Response({'shortened_url': os.path.join(APP_BASE_URL, serializer.data['id'])},
                         status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def redirect(request, short_id):
+    url = get_object_or_404(Urls, pk=short_id)
+    return HttpResponseRedirect(url.url)
